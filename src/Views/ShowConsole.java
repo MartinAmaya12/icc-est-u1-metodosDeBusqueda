631a0e9c571a0d2ba9fe9d2051bc707fcfd3ef4e
MartinAmaya12 <remasi12@hotmail.com>
2025-11-10 13:01:26 -0500

    Metodo de Busqueda

---
+++ src/Views/ShowConsole.java
@@ -0,0 +1,77 @@
+package Views;
+
+import models.Persona;
+
+public class ShowConsole {
+
+
+    public ShowConsole() {
+
+    }
+
+
+    public void printArray(int[] num) {
+        for(int i = 0; i< num.length; i++) {
+            System.out.print(num[i] + " ,");
+        }
+        System.out.println();
+    }
+
+
+    public void printResult(int result1) {
+        if(result1 == -1) {
+            System.out.println("Error -> No se encontro el valor");
+
+        } else {
+            System.out.println("Se encontro en la posicion:" + result1);
+        }
+    
+    }
+
+
+    public void printResult(int result1, int clave) {
+        if(result1 == -1) {
+            System.out.println("Error -> No se encontro el valor: " + "{" + clave + "}");
+
+        } else {
+            System.out.println("Se encontro el valor: " + "{" + clave + "}" +  " en la posicion:" + result1);
+        }
+    
+    }
+
+    public void printResult(Integer result4) {
+        if(result4 != null) {
+            System.out.println("Se encontro el valor:" + result4);
+
+        } else {
+            System.out.println("Error -> El valor buscado No se encontro");
+        }
+    }
+
+    public void printResult1(Integer result5) {
+        if(result5 == null) {
+            System.out.println("Error -> No se encontro el valor buscado ");
+
+        } else {
+            System.out.println("Se encontro el valor:" + "{" + result5 + "}");
+        }
+    }
+
+
+    public void showPersonResult(Persona resultadoPer, String name) {
+        System.out.println(
+            resultadoPer != null
+                ? "Se encontro la persona: " + resultadoPer
+                : "No se encontro a la persona: " + name
+            );
+    }
+    
+    public void showPersonResult(Persona resultadoPer2, int age) {
+        System.out.println(
+            resultadoPer2 != null
+                ? "Se encontro la persona: " + resultadoPer2
+                : "No se encontro a la persona con edad mayor a: " + age
+            );
+    }
+}
+
---
+++ src/controllers/MetodosBusqueda.java
@@ -0,0 +1,109 @@
+package controllers;
+import Views.ShowConsole;
+import models.Persona;
+
+
+
+public class MetodosBusqueda {
+    private ShowConsole showConsole;
+    private int[] arreglo;
+
+    public MetodosBusqueda() {
+        this.arreglo = new int[] { 10, 11, -2, 5, 6, 8, 15, 22} ;
+        this.showConsole = new ShowConsole();
+        showConsole.printArray(arreglo);
+
+        //Buscar el 5
+        int result1 = busquedaLinealWhile(5);
+        showConsole.printResult(result1, 5);
+
+        //Buscar el -2
+        
+        int result2 = busquedaLinealWhile(-2);
+        showConsole.printResult(result2, -2);
+
+        //Buscar el -20
+        int result3 = busquedaLinealWhile(20);
+        showConsole.printResult(result3, 20);
+
+        //Buscar el 100
+        //int result4 = busquedaLinealObj(100);      /*Esto dara un error porque int es entero y retorna null  */
+        //showConsole.printResult(result4, 100);
+
+        //Buscar el 100
+        Integer result4 = busquedaLinealObj(100);
+        showConsole.printResult(result4);
+
+        //Buscar el 100
+        Integer result5 = busquedaLinealObj(10); 
+        showConsole.printResult(result5);
+    }
+
+    ///Retorna la posicion del valor buscado
+    ///Si no encontro retorna -1
+    public int busquedaLineal(int clave) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == clave) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+    public int busquedaLinealWhile(int clave) {
+        int i = 0;
+        while (i < arreglo.length) {
+            if (arreglo[i] == clave) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+
+    ///Retorna el OBJETO buscado si lo encontro;
+    /// Si no encontro retorna NULL;
+    public Integer busquedaLinealObj(int clave) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == clave) {
+                return arreglo[i];
+            }
+        }
+        return null;
+    }
+
+    public Persona searchPersonByName(
+        Persona[] personas,
+        String name) {
+    for (Persona persona : personas) {
+        if(persona.getName().equals(name)){
+            return persona;
+        }
+            
+        
+    }        
+        return null;
+    }
+
+
+    //Encontrar una persona que sea mayor de: age 
+    //Y La edad sea impar
+    public Persona findPersonByAgeAnd(
+        Persona[] personas,
+        int age) {
+            for(Persona persona : personas){
+                if(persona.getAge() > age && persona.getAge() % 2 != 0){
+                    return persona;
+                }
+            }
+            //Siempre que se trate de objetos se debe hacer un return null sino no funcion y dira que metodo no retorna.
+            return null;
+        }
+        
+    
+    
+}
